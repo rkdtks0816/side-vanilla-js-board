7d2c298980935d011301abb1e7007e5842049473
rkdtks0816 <rkdtks0816@naver.com>
2022-12-26 17:26:46 +0900

    7

--- db_api/SongBoardService.py
+++ db_api/SongBoardService.py
@@ -20,30 +20,12 @@ def datetime_to_json_formatting_hour(o):
     if isinstance(o, (date, datetime)):
         return o.strftime('%Y%m%d%H')
 
-#############################################################################################################################################
-
+# DB 연동
 def GetConnection():
     connection = pymysql.connect(host='10.0.2.15', port=3306, user='root', password='1234',
                                     db='db_song', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
-    return connection
 
-# 아이디 정보 DB 저장   
-async def GetCreatID(UserName: str, NickName: str, UserID: str, UserPassword: str, UserEmail: str, IdCreatDatetime: str):
-    try:
-        connection = GetConnection()
-
-        with connection.cursor() as cursor:
-            query = f"""
-                INSERT INTO UserTable VALUES ( '{UserName}', '{NickName}', '{UserID}', '{UserPassword}', '{UserEmail}', '{IdCreatDatetime}' );
-            """
-
-            cursor.execute(query)
-            connection.commit()
-            _logger.Info(
-                f"succeed to do 'GetCreatIDTable('{UserName}', '{NickName}', '{UserID}', '{UserPassword}', '{UserEmail}', '{IdCreatDatetime}')'")
-
-    except Exception as ex:
-        _logger.Info(f"error to do 'GetCreatIDTable('{UserName}', '{NickName}', '{UserID}', '{UserPassword}', '{UserEmail}', '{IdCreatDatetime}')'")
+# Login #############################################################################################################################################
 
 # 아이디 중복확인    
 async def GetReadID(UserID: str):
@@ -59,7 +41,6 @@ async def GetReadID(UserID: str):
             cursor.execute(query)            
             
             rv = cursor.fetchall()
-
             json_data = json.dumps(rv, indent=4)
             
             _logger.Info(
@@ -69,3 +50,174 @@ async def GetReadID(UserID: str):
 
     except Exception as ex:
         _logger.Info(f"error to do 'GetReadID('{UserID}')'")
+    return connection
+    
+# 닉네임 중복확인    
+async def GetReadNickName(NickName: str):
+    try:
+        connection = GetConnection()
+
+        with connection.cursor() as cursor:
+            query = f"""
+                select NickName from UserTable
+                where NickName = '{NickName}';
+            """
+
+            cursor.execute(query)            
+            
+            rv = cursor.fetchall()
+            json_data = json.dumps(rv, indent=4)
+            
+            _logger.Info(
+                f"succeed to do 'GetReadNickName('{NickName}')'")
+            
+            return json_data
+
+    except Exception as ex:
+        _logger.Info(f"error to do 'GetReadNickName('{NickName}')'")
+    return connection
+
+# 아이디 정보 저장   
+async def GetCreatID(UserName: str, NickName: str, UserID: str, UserPassword: str, UserEmail: str, IdCreatDatetime: str):
+    try:
+        connection = GetConnection()
+
+        with connection.cursor() as cursor:
+            query = f"""
+                INSERT INTO UserTable VALUES ( '{UserName}', '{NickName}', '{UserID}', '{UserPassword}', '{UserEmail}', '{IdCreatDatetime}' );
+            """
+
+            cursor.execute(query)
+
+            connection.commit()
+
+            _logger.Info(
+                f"succeed to do 'GetCreatIDTable('{UserName}', '{NickName}', '{UserID}', '{UserPassword}', '{UserEmail}', '{IdCreatDatetime}')'")
+
+    except Exception as ex:
+        _logger.Info(f"error to do 'GetCreatIDTable('{UserName}', '{NickName}', '{UserID}', '{UserPassword}', '{UserEmail}', '{IdCreatDatetime}')'")
+
+# 아이디 정보 삭제  
+async def GetDeleteID(UserID: str):
+    try:
+        connection = GetConnection()
+
+        with connection.cursor() as cursor:
+            query = f"""
+                delete from UserTable where UserID = '{UserID}';
+            """
+
+            cursor.execute(query)
+
+            connection.commit()
+
+            _logger.Info(
+                f"succeed to do 'GetDeleteID('{UserID}')'")
+
+    except Exception as ex:
+        _logger.Info(f"error to do 'GetDeleteID('{UserID}')'")
+
+# Post #############################################################################################################################################
+
+# 게시글 저장   
+async def GetCreatPost(NickName: str, UserID: str, PostTitle: str, PostContent: str, PostCreatDatetime: str):
+    try:
+        connection = GetConnection()
+
+        with connection.cursor() as cursor:
+            query = f"""
+                INSERT INTO PostTable VALUES ( '{NickName}', '{UserID}', '{PostTitle}', '{PostContent}', '{PostCreatDatetime}' );
+            """
+
+            cursor.execute(query)
+
+            connection.commit()
+
+            _logger.Info(
+                f"succeed to do 'GetCreatPost('{NickName}', '{UserID}', '{PostTitle}', '{PostContent}', '{PostCreatDatetime}')'")
+
+    except Exception as ex:
+        _logger.Info(f"error to do 'GetCreatPost('{NickName}', '{UserID}', '{PostTitle}', '{PostContent}', '{PostCreatDatetime}')'")
+
+# 게시글 조회
+async def GetReadPost(NickName: str, PostTitle: str, PostCreatDatetime: str):
+    try:
+        connection = GetConnection()
+
+        with connection.cursor() as cursor:
+            query = f"""
+                select
+                    PostTitle,
+                    NickName,
+                    PostCreatDatetime,
+                    PostContent
+                from PostTable 
+                where 
+                    NickName = '{NickName}' and 
+                    PostTitle = '{PostTitle}' and 
+                    PostCreatDatetime = '{PostCreatDatetime}';
+            """
+
+            cursor.execute(query)       
+            
+            rv = cursor.fetchall()
+            json_data = json.dumps(rv, indent=4)
+            
+            _logger.Info(
+                f"succeed to do 'GetReadPost('{NickName}', '{PostTitle}', '{PostCreatDatetime}')'")
+            
+            return json_data
+
+    except Exception as ex:
+        _logger.Info(f"error to do 'GetReadPost('{NickName}', '{PostTitle}', '{PostCreatDatetime}')'")
+
+# 게시글 수정   
+async def GetUpdatePost(NickName: str, PostTitle: str, PostContent: str, PostCreatDatetime: str):
+    try:
+        connection = GetConnection()
+
+        with connection.cursor() as cursor:
+            query = f"""
+                UPDATE PostTable 
+                SET 
+                    PostTitle = '{PostTitle}', 
+                    PostContent = '{PostContent}'
+                WHERE
+                    NickName = '{NickName}' and
+                    PostCreatDatetime = '{PostCreatDatetime}';
+            """
+
+            cursor.execute(query)
+
+            connection.commit()
+
+            _logger.Info(
+                f"succeed to do 'GetUpdatePost('{NickName}', '{PostTitle}', '{PostContent}', '{PostCreatDatetime}')'")
+
+    except Exception as ex:
+        _logger.Info(f"error to do 'GetUpdatePost('{NickName}', '{PostTitle}', '{PostContent}', '{PostCreatDatetime}')'")
+
+
+# 게시글 삭제  
+async def GetDeletePost(NickName: str, PostCreatDatetime: str):
+    try:
+        connection = GetConnection()
+
+        with connection.cursor() as cursor:
+            query = f"""
+                delete 
+                from PostTable 
+                where
+                    NickName = '{NickName}' and
+                    PostCreatDatetime = '{PostCreatDatetime}';
+            """
+
+            cursor.execute(query)
+
+            connection.commit()
+
+            _logger.Info(
+                f"succeed to do 'GetDeletePost('{NickName}', '{PostCreatDatetime}')'")
+
+    except Exception as ex:
+        _logger.Info(f"error to do 'GetDeletePost('{NickName}', '{PostCreatDatetime}')'")
