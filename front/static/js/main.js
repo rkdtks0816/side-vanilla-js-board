3a0cb940fae493cb13240337d5a404155f7f1e78
rkdtks0816 <rkdtks0816@naver.com>
2022-12-27 16:42:54 +0900

    13

--- front/static/js/main.js
+++ front/static/js/main.js
@@ -14,10 +14,16 @@ function CheckSignUp(){
 
   if (NickName.length < 2 || NickName.length >= 20) {
     alert("닉네임은 2글자 이상 20글자 이하입니다.");
+  } else if (ReadNickNameCall(NickName) === 0) {
+    alert("중복된 닉네임입니다.");
   } else if (UserID.length < 2 || UserID.length >= 20) {
     alert("아이디는 2글자 이상 20글자 이하입니다.");
+  } else if (ReadIDCall(UserID) === 0) {
+    alert("중복된 아이디입니다.");
   } else if (UserPassword.length < 2 || UserPassword.length >= 20) {
     alert("비밀번호는 2글자 이상 20글자 이하입니다.");
+  } else if (UserPassword !== UserPasswordCheckword) {
+    alert("비밀번호가 다릅니다.");
   } else if (!UserEmail.includes("@") || !UserEmail.includes(".")) {
     alert("올바른 이메일 형식이 아닙니다.");
   } else {
