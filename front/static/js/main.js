1f7ad66a911ee3dc003a700b765246826e872c58
rkdtks0816 <rkdtks0816@naver.com>
2022-12-28 14:48:35 +0900

    18

--- front/static/js/main.js
+++ front/static/js/main.js
@@ -34,9 +34,32 @@ function CheckSignUp(){
 
 // Login #############################################################################################################################################
 
-// 유효성 검사
+// 쿠키
 
+// 쿠키 저장
+function setCookie(name, value, exp) {
+	var date = new Date();
+	date.setTime(date.getTime() + exp*24*60*60*1000);
+	document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+}
+
+// 쿠키 불러오기
+function getCookie(name) {
+	var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    
+	return value? value[2] : null;
+};
+
+// 쿠키 삭제하기
+function deleteCookie(name) {
+	document.cookie = name + '=; expires=Thu, 01 Jan 1999 00:00:10 GMT;';
+}
+
+// 유효성 검사
 function CheckLogin(){
+  var tomrrow = new Date();
+  tomrrow.setDate(tomrrow.getDate() + 1);
+
   var UserID = document.getElementById('UserID').value;
   var UserPassword = document.getElementById('UserPassword').value;
   var LoginCheck = LoginCheckCall(UserID, UserPassword);
@@ -48,8 +71,31 @@ function CheckLogin(){
     alert("비밀번호가 일치하지 않습니다.");
   } else if (LoginCheck === 2) {
     alert("로그인 되었습니다.");
+    setCookie('UserID', UserID, 1);
     location.replace("/");
   } else {
     alert("로그인 에러.");
   }
 }
+
+// 로그인
+function LoginLogout() {
+  if (getCookie('UserID') !== null) {
+    $('.login').append(`<a onclick="deleteCookie('UserID')" class="login_text">로그아웃</a>`)
+  } else {
+    $('.login').append(`<a href="/login" class="login_text">로그인</a>`)
+  }
+}
+
+// Post #############################################################################################################################################
+
+// 등록 버튼
+function writebt() {
+  if (getCookie('UserID') !== null) {
+    $('.board_list .bt_wrap').append(`<a href="/write" class="on">등록</a>`)
+  } else {
+    $('.board_list .bt_wrap').append(``)
+  }
+}
+
+// 게시물 리스트
