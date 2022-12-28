be15688f94b3f7bb319700ceca07b8bf9fa98682
rkdtks0816 <rkdtks0816@naver.com>
2022-12-28 19:51:26 +0900

    20

--- front/static/js/main.js
+++ front/static/js/main.js
@@ -100,6 +100,31 @@ function writebt() {
 }
 
 // 페이징
+function MainPaging () {
+  let totalData; //총 데이터 수
+  let dataPerPage = 10; //한 페이지에 나타낼 글 수
+  let pageCount = 10; //페이징에 나타낼 페이지 수
+  let globalCurrentPage = 1; //현재 페이지
+
+  $.ajax({ // ajax로 데이터 가져오기
+    method: "GET",
+    url: "https://url/data?" + data,
+    dataType: "json",
+    success: function (d) {
+      //totalData 구하기
+      totalData = d.data.length;
+    }
+  });
+  
+  //글 목록 표시 호출 (테이블 생성)
+  displayData(1, dataPerPage);
+  
+  //페이징 표시 호출
+  paging(totalData, dataPerPage, pageCount, 1);
+  
+}
+
+// 페이징
 function paging(totalData, dataPerPage, pageCount, currentPage) {
   console.log("currentPage : " + currentPage);
 
